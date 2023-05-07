3adaa4b7644e767f269b384d432b81b8a2790884
Sam <94141340+cmnowastaken@users.noreply.github.com>
2023-05-08 11:31:59 +1200

    another commit ig

--- pages/index.js
+++ pages/index.js
@@ -3,131 +3,124 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  function deezNuts() {
+    alert(
+      "can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i "
+    );
+  }
 
-function deezNuts () {
-  alert('can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i can i put my balls in yo jaws (yo jaws) balls in yo jaws can i can i can i can i ')
-}
-
-const horses = [
-  {
-      "id" : 1,
-      "name" : "Martha",
-      "bio" : "A submissive and breedable mare looking for a long-time partner."
-  },
-  {
-      "id" : 2,
-      "name" : "Derek",
-      "bio" : "A hot and sexy stallion who is ready to rumble."
-  },
-  {
-    "id" : 3,
-    "name" : "🥵Bingus🥵",
-    "bio" : "There are some great matches and good"
-  },
-  {
-      "id" : 4,
-      "name" : "Laura",
-      "bio" : "A fierce Gotland who won't take no for an answer."
-  },
-  {
-    "id" : 5,
-    "name" : "Björn",
-    "bio" : "A Scandanavian horse."
-  },
-  {
-    "id" : 6,
-    "name" : "Cynthia",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 7,
-    "name" : "Daddy Eater Jr.",
-    "bio" : "hi im god f*ck me please im so desperate"
-  },
-  {
-    "id" : 8,
-    "name" : "Vicky",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 9,
-    "name" : "Greg",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 10,
-    "name" : "Mac and Joe",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 11,
-    "name" : "Saul",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 12,
-    "name" : "Walter",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 13,
-    "name" : "Jesse",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 14,
-    "name" : "Frank",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 15,
-    "name" : "Tobes",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 16,
-    "name" : "Bertie",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 17,
-    "name" : "Gertrude",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 18,
-    "name" : "AAAAAAAAAA",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 19,
-    "name" : "Perry",
-    "bio" : "There are some great matches and good"
-  },
-  {
-    "id" : 20,
-    "name" : "Frances",
-    "bio" : "There are some great matches and good"
-  },
+  const horses = [
+    {
+      id: 1,
+      name: "Martha",
+      bio: "A submissive and breedable mare looking for a long-time partner.",
+    },
+    {
+      id: 2,
+      name: "Derek",
+      bio: "A hot and sexy stallion who is ready to rumble.",
+    },
+    {
+      id: 3,
+      name: "🥵Bingus🥵",
+      bio: "There are some great matches and good",
+    },
+    {
+      id: 4,
+      name: "Laura",
+      bio: "A fierce Gotland who won't take no for an answer.",
+    },
+    {
+      id: 5,
+      name: "Björn",
+      bio: "A Scandanavian horse.",
+    },
+    {
+      id: 6,
+      name: "Cynthia",
+      bio: "Likes long walks in the paddock.",
+    },
+    {
+      id: 7,
+      name: "Daddy Eater Jr.",
+      bio: "hi im god f*ck me please im so desperate",
+    },
+    {
+      id: 8,
+      name: "Vicky",
+      bio: "Loves Vicks VapoRub.",
+    },
+    {
+      id: 9,
+      name: "Greg",
+      bio: "Greg.",
+    },
+    {
+      id: 10,
+      name: "Mac and Joe",
+      bio: "A duo who loves to get freaky.",
+    },
+    {
+      id: 11,
+      name: "Saul",
+      bio: "Jimmy McGill.",
+    },
+    {
+      id: 12,
+      name: "Walter",
+      bio: "Jesse. We need to get ridden Jesse.",
+    },
+    {
+      id: 13,
+      name: "Jesse",
+      bio: "I agree Walter.",
+    },
+    {
+      id: 14,
+      name: "Frank",
+      bio: "I love pickles.",
+    },
+    {
+      id: 15,
+      name: "Tobes",
+      bio: "MY NAME ISNT TOBES",
+    },
+    {
+      id: 16,
+      name: "Bertie",
+      bio: "Bertieez nuts.",
+    },
+    {
+      id: 17,
+      name: "Gertrude",
+      bio: "There are some great matches and good",
+    },
+    {
+      id: 18,
+      name: "AAAAAAAAAA",
+      bio: "AAAAAAAAAA",
+    },
+    {
+      id: 19,
+      name: "Perry",
+      bio: "Perry is a bad name.",
+    },
+    {
+      id: 20,
+      name: "Frances",
+      bio: "I also love pickles.",
+    },
   ];
 
   return (
-    
-        
-      
-  <div id={`${styles.horsepics}`}>
-
-        {
-    horses.map(horse => (
-      <div key={horse.id}>
-        <div class={`${styles.name}`}>{horse.name}</div>
-        <img src={`${horse.id}.JPEG`} className={`${styles.horseImg}`} />      
-        <div class={`${styles.bio}`}>{horse.bio}</div>
-      </div>
-      ))
-  }
-
-</div>
-  
+    <div id={`${styles.horsepics}`}>
+      {horses.map((horse) => (
+        <div key={horse.id}>
+          <div class={`${styles.name}`}>{horse.name}</div>
+          <img src={`${horse.id}.JPEG`} className={`${styles.horseImg}`} />
+          <div class={`${styles.bio}`}>{horse.bio}</div>
+        </div>
+      ))}
+    </div>
   );
 }
